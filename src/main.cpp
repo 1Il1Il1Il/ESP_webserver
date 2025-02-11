0d355bf91d9ca37c555416d8ae524a28185b24b5
1Il1Il1Il <ovgithub@gmail.com>
2025-02-11 10:02:37 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,47 +1,21 @@
-#include <Arduino.h>
+#include "header.h"
 
-#include <ConfigSG.cpp>
+WiFiconfig wiFiconfig;
 
-#define SP_AP_NAME "ESP Config"
-#define SP_AP_IP 192,168,1,1
-
-#include <DNSServer.h>
-#ifdef ESP8266
-#include <ESP8266WiFi.h>
-#include <ESP8266WebServer.h>
-#else
-#include <WiFi.h>
-#include <WebServer.h>
-#endif
-
-#include <Adafruit_NeoPixel.h>
-
-static DNSServer _SP_dnsServer;
-#ifdef ESP8266
-static ESP8266WebServer _SP_server(80);
-#else
-static WebServer _SP_server(80);
-#endif
-
-void StartConfPortal();
-
-
+timer Timer1(1000, 0);
 
 void setup(){
-Serial.begin(115200);
-for (int i = 0; i < 10; i++) Serial.println();
-Serial.println("#######################################################################################");
-Serial.println();
+    Serial.begin(9600);
+    Serial.printf("\n\n\n\n\n################################## START ##################################\n\n");
 
-Config config(1024);
-
-config.Add({0,1,245,152,12}, "SSid");
-//config.Add("DDDD", "pasass");
-
-config.Save();
-
+    wiFiconfig.startWP();
+    Timer1.start();
 }
 
 void loop(){
-
+    wiFiconfig.tick();
+    if (Timer1.status()){
+        Serial.printf("\n1");
+    }
 }
+
