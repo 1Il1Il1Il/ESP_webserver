d89631c0964af535c0944bf9168a59cf46339195
1Il1Il1Il <ovgithub@gmail.com>
2025-02-13 21:54:20 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,7 @@
 #include "header.h"
 
 AccessPoint accessPoint;
+MainPade mainPage;
 timer restartAPtime(1000*60*15, 3);
 
 timer Timer1(1000, 0);
@@ -10,6 +11,7 @@ void setup(){
     Serial.printf("\n\n\n\n\n################################## START ##################################\n\n");
 
     EEPROM.begin(512);
+    pinMode(2, 1);
 
     getEEPROMdata();
 
@@ -17,24 +19,19 @@ void setup(){
     Serial.printf("\n%i.%i.%i.%i", ip[0], ip[1], ip[2], ip[3]);
     Serial.printf("\n%i.%i.%i.%i", gateway[0], gateway[1], gateway[2], gateway[3]);
     Serial.printf("\n%i.%i.%i.%i", subnet[0], subnet[1], subnet[2], subnet[3]);
-    
-    //startMP();
-    accessPoint.startCP();
+
+    mainPage.start(100);
     Timer1.start();
-    
-    static ESP8266WebServer server(80);
-    server.on("/connect", HTTP_POST, error);
 }
 
 void loop(){
-    delay(10000);
+    mainPage.tick();
+    // delay(10000);
     
-    accessPoint.tick();
-    if (restartAPtime.status()){
-        accessPoint.stop();
-        accessPoint.startCP();
-        Serial.printf("\nAP has restarted");
-    }
+    // accessPoint.tick();
+    // if (restartAPtime.status()){
+    //     accessPoint.stop();
+    //     accessPoint.startCP();
+    //     Serial.printf("\nAP has restarted");
+    // }
 }
-
-void error(){};
