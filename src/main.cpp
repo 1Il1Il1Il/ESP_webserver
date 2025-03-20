eecc2da5b9877e58ecc0514e4d8c75e702520391
1Il1Il1Il <ovgithub@gmail.com>
2025-03-20 19:31:02 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,37 +1,61 @@
+#define TRYCONN 150
+
 #include "header.h"
 
 AccessPoint accessPoint;
-MainPade mainPage;
+MainPage mainPage;
 timer restartAPtime(1000*60*15, 3);
+timer checkWiFi(1000*10, 3);
 
-timer Timer1(1000, 0);
+byte _WiFimode = 0;
+bool APflag = true;
 
 void setup(){
     Serial.begin(9600);
     Serial.printf("\n\n\n\n\n################################## START ##################################\n\n");
 
     EEPROM.begin(512);
-    pinMode(2, 1);
 
     getEEPROMdata();
 
     Serial.printf("\nSSid :\t%s\nPassword :\t%s", SSid, pass);
     Serial.printf("\n%i.%i.%i.%i", ip[0], ip[1], ip[2], ip[3]);
     Serial.printf("\n%i.%i.%i.%i", gateway[0], gateway[1], gateway[2], gateway[3]);
-    Serial.printf("\n%i.%i.%i.%i", subnet[0], subnet[1], subnet[2], subnet[3]);
+    Serial.printf("\n%i.%i.%i.%i\n", subnet[0], subnet[1], subnet[2], subnet[3]);
 
-    mainPage.start(100);
-    Timer1.start();
+    accessPoint.stop();
+    mainPage.stop();
+
+    fetchCurrentTime();
 }
 
 void loop(){
+    HandleConnect();
+}
+
+void HandleConnect(){
     mainPage.tick();
-    // delay(10000);
-    
-    // accessPoint.tick();
-    // if (restartAPtime.status()){
-    //     accessPoint.stop();
-    //     accessPoint.startCP();
-    //     Serial.printf("\nAP has restarted");
-    // }
+    accessPoint.tick();
+
+    if (_WiFimode == 0) {
+        _WiFimode = mainPage.start(TRYCONN);
+        APflag = false;
+    }
+
+    if (_WiFimode == 1) {
+        _WiFimode = mainPage.start(TRYCONN);
+    }
+
+    if (_WiFimode == 3 && !APflag) {
+        mainPage.reset();
+        accessPoint.startCP();
+        restartAPtime.start();
+        APflag = true;
+    }
+
+    if (accessPoint.gotStatus() || (restartAPtime.status() && _WiFimode == 3) || (checkWiFi.status() && _WiFimode == 4 && !mainPage.status())) {
+        accessPoint.stop();
+        mainPage.reset();
+        _WiFimode = 0;
+    }
 }
