132a915b9c0c1d83eaaad035a7ab49297d5f6448
1Il1Il1Il <ovgithub@gmail.com>
2025-04-05 17:43:08 +0300

    Update adresses.h

--- include/adresses.h
+++ include/adresses.h
@@ -1,6 +1,56 @@
-#define aSSid 0 
-#define apass 32 
+#define aSSid 0
+#define apass 32
 
-#define aip 64 
-#define agateway 68 
-#define asubnet 72 
+#define aip 64
+#define agateway 68
+#define asubnet 72
+#define adns 76
+#define adns2 80
+
+#define antpServer 84
+#define aperiodTime 184
+#define abrightnessRange 188
+#define astaticColor 192
+#define aspectrumSpeed 196
+#define agradientShift 200
+#define agradientSize 204
+#define aperiodHour 208
+#define alavaIntensity 212
+#define acelsiusColor 216
+#define apercentageColor 220
+
+#define aSSidSize 32
+#define apassSize 32
+#define aipSize 4
+#define agatewaySize 4
+#define asubnetSize 4
+#define adnsSize 4
+#define adns2Size 4
+#define antpServerSize 100
+#define aperiodTimeSize 4
+#define abrightnessRangeSize 4
+#define astaticColorSize 4
+#define aspectrumSpeedSize 4
+#define agradientShiftSize 4
+#define aperiodHourSize 4
+#define alavaIntensitySize 4
+#define acelsiusColorSize 4
+#define apercentageColorSize 4
+
+#define astaticCheckbox 224
+#define aspectrumCheckbox 225
+#define agradientCheckbox 226
+#define aperiodCheckbox 227
+#define alavaMode 228
+#define acelsiusColorCheckbox 229
+#define apercentageColorCheckbox 230
+#define ablinkPointCheckbox 231
+
+#define astaticCheckboxSize 1
+#define aspectrumCheckboxSize 1
+#define agradientCheckboxSize 1
+#define aperiodCheckboxSize 1
+#define alavaModeSize 1
+#define acelsiusColorCheckboxSize 1
+#define apercentageColorCheckboxSize 1
+#define ablinkPointCheckboxSize 1
